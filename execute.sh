a35e5076d4c84269fc1ec12c5b76b8e96efc78e1
cglosner <cglosner@gmail.com>
2023-03-30 10:13:48 -0400

    Can now create a checkpoint

--- execute.sh
+++ execute.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 main_dir=$PWD
+simics_dir=$main_dir/simics/fuzzer-project
 # Create a snapshot of the FW at the EFI Shell
+cd $simics_dir
+./simics -no-win -batch-mode targets/qsp-x86/qsp-modern-core.simics -e "run-seconds seconds = 30" -e "write-configuration shell-restore.conf"
 
 # run the fuzzer
-cd $main_dir/simics/fuzzer-project
 $main_dir/AFLplusplus/afl-fuzz -i testcases/ -o testrun/ -n -t 100000 -- ./afl-wrapper @@
