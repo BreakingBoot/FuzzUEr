b21dfd6c31e18210732c4895ec410da1b1fc439e
cglosner <cglosner@gmail.com>
2023-04-01 23:29:46 -0400

    Added support for taking an input integer

--- execute.sh
+++ execute.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+  echo "Please provide an argument."
+  exit 1
+fi
+
+# Check if the argument is an integer
+if ! [[ $1 =~ ^-?[0-9]+$ ]]; then
+  echo "Please provide an integer."
+  exit 1
+fi
+
+# Check if the argument is greater than or equal to 0 and less than 5
+if ! [[ $1 -ge 0 ] && [ $1 -lt 5 ]]; then
+  echo "The argument is outside the specified range."
+  exit 1
+fi
+
 main_dir=$PWD
 simics_dir=$main_dir/simics/fuzzer-project
 
 # run the fuzzer
 cd $simics_dir
-$main_dir/AFLplusplus/afl-fuzz -i testcases/ -o testrun/ -n -t 100000 -- ./afl-wrapper @@
+$main_dir/AFLplusplus/afl-fuzz -i testcases/ -o fuzz_output/ -n -t 100000 -- ./afl-wrapper $1 @@
--- run.sh
+++ run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Check if an argument is provided
+if [ $# -eq 0 ]; then
+  echo "Please provide an argument."
+  exit 1
+fi
+
+# Check if the argument is an integer
+if ! [[ $1 =~ ^-?[0-9]+$ ]]; then
+  echo "Please provide an integer."
+  exit 1
+fi
+
+# Check if the argument is greater than or equal to 0 and less than 5
+if ! [[ $1 -ge 0 ] && [ $1 -lt 5 ]]; then
+  echo "The argument is outside the specified range."
+  exit 1
+fi
+
 # install libraries
 ./install_libraries.sh
 
@@ -10,4 +28,4 @@
 ./compile.sh 
 
 # fuzz UEFI
-./execute.sh 
+./execute.sh $1
