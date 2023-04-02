a80c47d2dbf860aba19aa7743866a6d21de2bbc3
cglosner <cglosner@gmail.com>
2023-04-01 23:14:55 -0400

    Added Log support and removed some extra comments

--- afl-simics-linker.py
+++ afl-simics-linker.py
@@ -2,6 +2,7 @@ from simics import *
 import os
 import sys
 import time
+import signal
 
 #SIM_continue() # Continue Simulation
 #SIM_run_command_file(filename) # Run a command rile
@@ -9,34 +10,40 @@ import time
 #cli.quiet_run_command(input) # Run a CLI command, same as directly inputting into the terminal
 def fuzz(input):
     # Create  Input
+    input = input[:-7]
     fuzz_input = "BBClient.efi " + input + "\n"
+    
+    # Save to log file
+    with open("log.txt", "a") as f:
+        f.write("INPUT: " + fuzz_input)
 
     # Start simics
     # Set up serial output
-    cli.quiet_run_command('board.serconsole.con.capture-start -overwrite "console-output.txt"')
+    cli.quiet_run_command('board.serconsole.con.capture-start "log.txt"')
+
     # Set up code coverage
     try:
         cli.quiet_run_command('board.software.tracker.detect-parameters -load')
         cli.quiet_run_command('board.software.enable-tracker')
     except:
-        print("Ran into som error with Tracker")
+        with open("log.txt", "a") as f:
+            f.write("ERROR: Couldn't set up simics tracker\n")
         
+    # Set up code coverage
     cli.quiet_run_command('$cc = (collect-coverage context-query="\'UEFI Firmware\'")')
     try:
         cli.quiet_run_command('$cc.add-report \"coverage\"')
     except:
-        print("No coverage report to load")
-    #print("started code coverage")
+        with open("log.txt", "a") as f:
+            f.write("ERROR: Couldn't load existing code coverage report\n")
+          
     # Normally add a path map, but I don't need one since I am in the same system
-
+    # Run
     cli.quiet_run_command('board.console.con.input \"'+fuzz_input+'\"')
-    #SIM_stop()
+
     # wait until simics is finally running after start
     if (not SIM_simics_is_running()):
-        # Continue the simulation
-    #    SIM_continue(0)
-        cli.run_command("run-seconds seconds = 10")
-        #sleep
+        cli.quiet_run_command("run-seconds seconds = 1")
 
     
     # Stop code coverage capture and create a report
@@ -46,16 +53,22 @@ def fuzz(input):
     os.system("rm merged_tracefile.info")
     cli.quiet_run_command('$cc.lcov-output coverage-lcov')
     os.system("lcov_tracefile=$(ls coverage-lcov | sed -e \"s/^/-a coverage-lcov\//\") && lcov $lcov_tracefile -o merged_tracefile.info")
-    #print("generated report")
 
     # Stop serial output
     cli.quiet_run_command('board.serconsole.con.capture-stop')
-    #cli.quiet_run_command('restart-simics shell-restore.conf')
-
+    bp = cli.quiet_run_command('bp.show id = 1')
+    with open("log.txt", "a") as f:
+        f.write(str(bp)+"\n")
+    
+    # If the breakpoint is triggered then save the input into a crash log
+    # and then exit with SIGABRT so AFL can detect a crash
+    if int(bp[1].split(":")[-1]) > 0:
+        with open("crashes.txt", "a") as f:
+            f.write(fuzz_input + " " + str(bp)+"\n")
+        sys.exit(signal.SIGABRT)
 
 def main():
-    input = sys.argv[len(sys.argv)-1]
-    #print(input)
+    input = sys.argv[-1]
     fuzz(input)
 
 if __name__ == "__main__":
