c43f72dc1d43827ae0764c07479bba2a2dfe3950
cglosner <cglosner@gmail.com>
2023-04-01 23:06:19 -0400

    Will send an SIGABRT signal to AFL if Simics breakpoint is triggered

--- afl-wrapper.c
+++ afl-wrapper.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <signal.h>
 
-void fuzz(char* input)
+int fuzz(char* input)
 {
-    char* cmd = "./simics -no-win -c \"shell-restore.conf\" -batch-mode -e \"bp.console_string.break object = board.serconsole.con string = Error\"-p afl-simics-linker.py ";
+    char* cmd = "./simics -no-win -c \"shell-restore.conf\" -batch-mode -e \"bp.console_string.break object = board.serconsole.con string = FAILED\" -p afl-simics-linker.py ";
     int len = strlen(input);
     char* final_command = malloc(strlen(cmd) + len);
     memcpy(final_command, cmd, strlen(cmd));
@@ -13,21 +14,28 @@ void fuzz(char* input)
     fwrite(final_command, strlen(final_command), 1, out);
     fwrite("\n", 1, 1, out);
     fclose(out);
-    system(final_command);
+    int status = system(final_command);
+    int exit = WEXITSTATUS(status);
+    if(exit == 6)
+    {
+        raise(SIGABRT);
+    }
+    return 0;
 }
 
 int main(int argc, char* argv[])
 {
     if(argc > 0)
     {
-        char* filename = argv[1];
+        char* filename = argv[2];
         FILE *fp = fopen(filename, "r");
         fseek(fp, 0, SEEK_END);
         int size = ftell(fp);
         fseek(fp, 0, SEEK_SET);
-        char buffer[size];
-        fread(buffer, size, 1, fp);
-        fuzz(buffer);
+        char buffer[size+1];
+        memcpy(buffer, argv[1], sizeof(char));
+        fread(buffer+1, size, 1, fp);
+        return fuzz(buffer);
     }
     return 0;
 }
